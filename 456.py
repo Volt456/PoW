aea908872941aa12fb9e71350a76f65c189d2636
Andrey <andreybogky@gmail.com>
2025-08-08 17:12:14 +1000

    на

---
+++ 456.py
@@ -0,0 +1,57 @@
+# x= input("Хочешь пиццу?:")
+# y= input("Точно?:")
+# z= input("Точно?:")
+# b = []
+# b.append(x)
+# b.append(y)
+# b.append(z)
+# print("Количествво согласий:",b.count("да"))
+# print("Количествво отказов:",b.count("нет"))
+# x=[1,1000,13,100]
+# if x[0]>=x[1] and x[0]>=x[2] and x[0]>=x[3]:
+#     print(x[0])
+# elif x[1]>=x[0] and x[1]>=x[2] and x[1]>=x[3]:
+#     print(x[1])
+# elif x[2]>=x[0] and x[2]>=x[1] and x[2]>=x[3]:
+#     print(x[2])
+# elif x[3]>=x[0] and x[3]>=x[1] and x[3]>=x[2]:
+#     print(x[3])
+# x=[1,2,34,5]
+# minimal=999999999999999999
+# if x[0]<=minimal:
+#     minimal=x[0]
+# if x[1]<=minimal:
+#     minimal=x[1]
+# if x[2]<=minimal:
+#     minimal=x[2]
+# if x[3]<=minimal:
+#     minimal=x[3]
+# print(minimal)
+
+
+# x=[-19,2,13,4]
+# maxim=-999999999999999999999999
+# if x[0]>=maxim:
+#      maxim=x[0]
+# if x[1]>=maxim:
+#      maxim=x[1]
+# if x[2]>=maxim:
+#      maxim=x[2]
+# if x[3]>=maxim:
+#      maxim=x[3]
+# minimal=999999999999999999
+# if x[0]<=minimal:
+#     minimal=x[0]
+# if x[1]<=minimal:
+#     minimal=x[1]
+# if x[2]<=minimal:
+#     minimal=x[2]
+# if x[3]<=minimal:
+#     minimal=x[3]
+# if (x[0]+x[1]+x[2]+x[3])>2*maxim:
+#     print(minimal)
+
+
+
+
+
